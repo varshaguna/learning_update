c767183eea3da069ac011b541ce93a3a7ef5e4c4
varshaguna <129100249+varshaguna@users.noreply.github.com>
2024-11-13 13:05:38 +0530

    Create withpointers.go

---
+++ withpointers.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 32
+
+	var userAge *int
+	userAge = &age
+
+	fmt.Println("Age:", *userAge)
+
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears) 
+
+}
+
+func getAdultYears(age int) int{
+	return age - 18
+}
