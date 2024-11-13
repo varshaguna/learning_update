43bb422d823e4c6dc9c8a4b2ec9bc0c361830294
varshaguna <129100249+varshaguna@users.noreply.github.com>
2024-11-13 12:54:24 +0530

    Create withoutpointers.go

---
+++ withoutpointers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	age := 32
+
+	fmt.Println("Age:", age)
+
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears) 
+
+}
+
+func getAdultYears(age int) int{
+	return age - 18
+}
