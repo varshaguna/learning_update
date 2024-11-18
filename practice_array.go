a1c7a41b57c151994ad6d252d6d415b0938b8d9d
varshaguna <129100249+varshaguna@users.noreply.github.com>
2024-11-18 19:13:07 +0530

    Update practice_array.go

--- practice_array.go
+++ practice_array.go
@@ -7,72 +7,68 @@ import "fmt"
 type Product struct {
 	id string
 	title string
-	price float64
+	prices float64
 }
 
 func main() {
-	hobbies := [3]string{"playing", "skipping", "eating"}
+	hobbies := [3]string{"dancing", "planting", "skipping"}
 	fmt.Println(hobbies)
 
 
 //Also output more data about that array:
 //		- The first element (standalone)
 //		- The second and third element combined as a new list
-
-    fmt.Println(hobbies[0])
-	fmt.Println(hobbies[1:3])
+     fmt.Println(hobbies[0])
+	 fmt.Println(hobbies[1:3])
 
 //      Create a slice based on the first element that contains
 //		the first and second elements.
 //		Create that slice in two different ways (i.e. create two slices in the end)
-
-    mainHobbies := hobbies[:2]
-	fmt.Println(mainHobbies)
-
+      mainhobbies := hobbies[:2]
+	  fmt.Println(mainhobbies)
+    
 //   Re-slice the slice from (3) and change it to contain the second
 //		and last element of the original array.
-
-     fmt.Println(cap(mainHobbies))
-	 mainHobbies = mainHobbies[1:3]
-	 fmt.Println(mainHobbies)
-
+       fmt.Println(cap(mainhobbies))
+	   mainhobbies = mainhobbies[1:3]
+	   fmt.Println(mainhobbies)
+    
     // Create a "dynamic array" that contains your course goals (at least 2 goals)
 
-     courseGoals := []string{"Learn Go!", "Learn all the basics"}
-	 fmt.Println(courseGoals)
+       courseGoals := []string{"Be positive", "Exercise Weekly Once"}
+	   fmt.Println(courseGoals)
 
 	 // Set the second goal to a different one AND then add a third goal to that existing dynamic array
-	 courseGoals[1] = "Learn all the details!"
-	 courseGoals = append(courseGoals, "Learn all the basics!")
-	 fmt.Println(courseGoals)
-
+	 
+	   courseGoals[1] = "Exercise Daily"
+	   courseGoals = append(courseGoals, "Be a GoodFit")
+	   fmt.Println(courseGoals)
 	 // Bonus: Create a "Product" struct with title, id, price and create a
      //		dynamic list of products (at least 2 products).
      //		Then add a third product to the existing list of products.
-
+ 
 	 products := []Product{
 		{
-			"first-product",
-			"A First Product",
-			12.99,
+			"first pro",
+			"A first product",
+			9.2,
 		},
 		{
-			"second-product",
-			"A Second Product",
-			129.99,
+			"second pro",
+			"A second product",
+			7.3,
 		},
 	 }
 
 	 fmt.Println(products)
 
 	 newProduct := Product{
-		"third-product",
-		"A Third Product",
-		15.99,
+		"third pro",
+		"A third product",
+		9.3,
 	 }
-   
+
 	 products = append(products, newProduct)
 
-   
 	 fmt.Println(products)
 }
