151d1dc7c96da14ad26d6224ba0f71846f444172
varshaguna <129100249+varshaguna@users.noreply.github.com>
2024-11-18 12:19:04 +0530

    Create array_slices.go

---
+++ array_slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productNames [4]string = [4]string{"A Book"}
+	prices := [4]float64{8.5, 12.2, 45.4, 88.7}
+	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
+
+	fmt.Println(productNames)
+
+	fmt.Println(prices[2])
+
+	featuredPrices := prices[2:4]
+
+	fmt.Println(featuredPrices)
+}
