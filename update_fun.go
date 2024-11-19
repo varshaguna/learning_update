0b66a6dc1d8a6212a548d20c453c9d5b40d4a66e
varshaguna <129100249+varshaguna@users.noreply.github.com>
2024-11-19 11:42:31 +0530

    Create update_fun.go

---
+++ update_fun.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//type trasformFn func(int) int
+
+func main(){
+	numbers := []int{1, 2, 3, 4}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transform func(int) int) []int{
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+
+	return dNumbers
+}
+
+func double(number int) int{
+	return number*2
+}
+
+func triple(number int) int{
+	return number*3
+}
